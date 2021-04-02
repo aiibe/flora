0a226963da748fe1a22254ecc8b4541944cf1336
aiibe <souksavanh.sy@gmail.com>
2021-04-02 16:02:28 +0200

    logo

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,14 @@
 <script>
+	// import Logo from './Logo.svelte'
 	export let segment;
 </script>
 
 <nav class="navbar has-shadow">
+	<div class="container">
+		<div class="navbar-brand">
+			<a href="." class="navbar-item">
+				<img src="/flora.svg" alt="flora logo" width="120">
+			</a>
+		</div>
+	</div>
 </nav>
